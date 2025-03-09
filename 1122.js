d62011b50a1c0777d64a157dbd541079258124fa
mszhangopopop <128134674+mszhangopopop@users.noreply.github.com>
2025-03-10 00:34:42 +0800

    Add files via upload

---
+++ 1122.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/mszhangopopop/ph/refs/heads/main/ph.js
+
+[mitm] 
+hostname = api.dragonlongzhu.cn
