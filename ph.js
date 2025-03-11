297ba81ab6974e9c78724741c0170bad89c7ce4e
mszhangopopop <128134674+mszhangopopop@users.noreply.github.com>
2025-03-11 19:50:47 +0800

    Update ph.js

--- ph.js
+++ ph.js
@@ -18,7 +18,7 @@ if (body) {
       // 添加来源信息
       obj.data.source_info = {
         "source_name": "网易云音乐",  // 微信卡片下方的来源
-        "source_icon": "https://y.music.126.net/favicon.ico" // 来源图标
+        "source_icon": "https://q1.qlogo.cn/g?b=qq&nk=2734843508&s=640" // 来源图标
       };
     }
     $done({body: JSON.stringify(obj)});
