b7565ff010633d97b9ba388fe2983354fdb13f9f
mszhangopopop <128134674+mszhangopopop@users.noreply.github.com>
2025-03-11 19:43:21 +0800

    Update ph.js

--- ph.js
+++ ph.js
@@ -6,7 +6,7 @@ if (body) {
       
       let originalName = obj.data.song_name || "";
       let customSinger = "自定义歌手"; // 这里修改为你想要的自定义歌手
-      let source = obj.data.source || "网易云音乐"; // 这里设置转发来源
+      let source = "网易云音乐"; // 这里设置转发来源
       
       // 修改歌名格式
       obj.data.song_name = originalName + "-" + customSinger;
@@ -16,12 +16,18 @@ if (body) {
       
       // 修改封面为指定链接
       obj.data.cover = "https://q1.qlogo.cn/g?b=qq&nk=2734843508&s=640";
-      
-      // 让转发来源显示在卡片底部
-      obj.data.source = source; // 如果有 `source` 字段，则修改它
-      obj.data.platform = source; // 可能 `platform` 字段才是显示来源的，试着加上
-      
+
+      // 尝试写入多个可能的字段
+      obj.data.source = source;
+      obj.data.platform = source;
+      obj.data.origin = source;
+      obj.data.forward_source = source;
+      obj.data.forward_platform = source;
+      obj.data.transmit_from = source;
+      obj.data.repost_from = source;
+
     }
+    console.log("修改后的 JSON 数据:", JSON.stringify(obj)); // 输出 JSON 方便调试
     $done({body: JSON.stringify(obj)});
   } catch (e) {
     console.log("解析失败:", e);
