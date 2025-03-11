55d040d52b8cb864ffe589fe76f0f3daced194ca
mszhangopopop <128134674+mszhangopopop@users.noreply.github.com>
2025-03-11 19:39:47 +0800

    Update ph.js

--- ph.js
+++ ph.js
@@ -5,11 +5,10 @@ if (body) {
     if (obj && obj.data) {
       
       let originalName = obj.data.song_name || "";
+      let source = obj.data.source || "未知来源"; // 获取转发来源，若无则显示"未知来源"
+      let customSinger = "自定义歌手"; // 这里修改为你想要的自定义歌手
       
-      // 允许自定义歌手
-      let customSinger = "胖虎"; // 这里修改为你想要的自定义歌手
-      
-      obj.data.song_name = originalName + " - " + customSinger;
+      obj.data.song_name = originalName + "-" + customSinger + " [" + source + "]";
       // 将歌手改为固定文本
       obj.data.song_singer = "胖虎yyds";
       // 修改封面为指定链接
