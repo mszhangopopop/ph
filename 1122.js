c2ab3911c08b86fc0890230ada20e22e2ecb2e4b
mszhangopopop <128134674+mszhangopopop@users.noreply.github.com>
2025-03-15 14:37:02 +0800

    Update 1122.js

--- 1122.js
+++ 1122.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/mszhangopopop/ph/refs/heads/main/ph.js
+^https:\/\/(api\.dragonlongzhu\.cn|www\.hhlqilongzhu\.cn|api\.dragonlongzhu\.cn)\/.* url script-response-body https://raw.githubusercontent.com/mszhangopopop/ph/refs/heads/main/ph.js
 
-[mitm] 
-hostname = api.dragonlongzhu.cn
+[mitm]
+hostname = api.xingzhige.com, www.hhlqilongzhu.cn, api.dragonlongzhu.cn
