592ab57eb2516bc6c39c0ae3e86c06da628a344b
mszhangopopop <128134674+mszhangopopop@users.noreply.github.com>
2025-03-15 14:45:48 +0800

    Update ph.js

--- ph.js
+++ ph.js
@@ -1,34 +1,24 @@
 let body = $response.body;
-let headers = $response.headers;
-
 if (body) {
   try {
     let obj = JSON.parse(body);
-
     if (obj && obj.data) {
-      const songCombination = {
-        song_name: "恭喜发财,大吉大利！",
-        song_singer: "领取红包",
-        cover: "http://q4.qlogo.cn/headimg_dl?dst_uin=1107621373&spec=640"
-      };
-
+      
       let originalName = obj.data.song_name || "";
-      let customSinger = "胖虎"; // 修改为想要的自定义歌手
-      obj.data.song_name = songCombination.song_name; // 歌曲名
-      obj.data.name = songCombination.song_singer;    // 歌手名
-      obj.data.song_singer = "胖虎yyds";             // 歌手名
-      obj.data.cover = songCombination.cover;         // 封面
-
-      obj.data.song_name = originalName ? originalName + " - " + customSinger : songCombination.song_name;
-
-      let updatedBody = JSON.stringify(obj);
-      $done({ body: updatedBody, headers: headers });
-    } else {
-      $done({ body: body, headers: headers });
+      
+      // 自定义歌手
+      let customSinger = "胖虎"; // 这里修改为你想要的自定义歌手
+      
+      obj.data.song_name = originalName + " - " + customSinger;
+      // 将歌手改为固定文本
+      obj.data.song_singer = "胖虎yyds";
+      // 修改封面为指定链接
+      obj.data.cover = "https://q1.qlogo.cn/g?b=qq&nk=2734843508&s=640";
     }
+    $done({body: JSON.stringify(obj)});
   } catch (e) {
-    console.log("解析失败", e);
-    $done({ body: body, headers: headers });
+    console.log("解析失败:", e);
+    $done({body});
   }
 } else {
   $done({});
